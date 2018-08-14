6a7da8b94b25fb7802bfe67a4812dee377bfaa86
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-08-14 11:37:24 +0530

    Update and rename README.md to bitwise.java

---
+++ bitwise.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Swap_BitwiseXOR 
+{
+    public static void main(String args[])
+    {
+        int m, n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        m = s.nextInt();
+        System.out.print("Enter the second number:");
+        n = s.nextInt();
+        m = m ^ n;
+        n = m ^ n;
+        m = m ^ n;
+        System.out.println("After Swapping");
+        System.out.println("First number:"+m);
+        System.out.println("Second number:"+n);
+    }
+}
+
+
